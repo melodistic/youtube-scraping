9e160856996545ea343f1952a3a8df6fcd8d1162
GGolfz <wisarutgolf11@gmail.com>
2022-01-16 13:55:07 +0700

    add: detect song language to be english :sparkles:

--- generate_json.py
+++ generate_json.py
@@ -1,6 +1,7 @@
 import json
 import os
 from ytmusicapi import YTMusic
+from langdetect import detect
 yt = YTMusic()
 
 def get_playlist_by_param(param):
@@ -11,7 +12,11 @@ def get_tracks(param):
     track = yt.get_playlist(param)["tracks"]
     data = []
     for i in track:
-        data.append({"id": i["videoId"], "title": i["title"]})
+        try:
+            if detect(i["title"]) == "en":
+                data.append({"id": i["videoId"], "title": i["title"]})
+        except:
+            continue
     return data
 
 if __name__ == "__main__":
